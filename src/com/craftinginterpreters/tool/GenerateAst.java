1ad73be659c83dea925a7bdbeace4ba8214dabe2
JJatau <jjatau14@outlook.com>
2025-09-22 00:09:44 +0100

    update GenerateAst method to use visitor pattern

--- src/com/craftinginterpreters/tool/GenerateAst.java
+++ src/com/craftinginterpreters/tool/GenerateAst.java
@@ -31,11 +31,17 @@ public class GenerateAst {
     writer.println();
     writer.println("abstract class " + baseName + " {");
 
+    defineVisitor(writer, baseName, types);
+
     for(String type: types) {
       String className = type.split(":")[0].trim();
       String fields = type.split(":")[1].trim();
       defineType(writer, baseName, className, fields);
     }
+
+    // base accept method
+    writer.println();
+    writer.println(" abstract <R> R accept(Visitor<R> visitor);");
 
     writer.println("}");
     writer.close();
@@ -61,6 +67,23 @@ public class GenerateAst {
     }
 
     writer.println(" }");
+
+    writer.println();
+    writer.println(" @Override");
+    writer.println(" <R> R accept(Visitor<R> visitor) {");
+    writer.println(" return visitor.visit" + className + baseName + "(this);");
+    writer.print(" }");
+
+    writer.println(" }");
+  }
+
+  private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+    writer.println(" interface Visitor<R> {");
+
+    for(String type : types) {
+      String typeName = type.split(":")[0].trim();
+      writer.println(" R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase() + ");");
+    }
 
     writer.println(" }");
   }
